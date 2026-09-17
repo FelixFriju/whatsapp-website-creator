9a60969697b0db4d04f9298a28e5784c98904c68
Felix Friju <YOUR_GITHUB_EMAIL>
2026-09-17 13:14:10 +0530

    Fix template exports

--- components/templates/website1/index.ts
+++ components/templates/website1/index.ts
@@ -1,17 +1,21 @@
 export { default, BusinessWebsiteTemplate } from "./Template";
-export type { TemplatePage } from "./Template";
 
 export { Home } from "./Home";
 export { About } from "./About";
 export { Products } from "./Products";
 export { Contact } from "./Contact";
+
 export { Header } from "./Header";
+
 export type { NavItem } from "./links";
+
 export { Footer } from "./Footer";
+
 export { ProductModal } from "./ProductModal";
 export { ProductGrid } from "./ProductGrid";
 
 export { withTemplateProducts } from "./data";
+
 export type { Product, WebsiteData } from "./data";
 
 export {
